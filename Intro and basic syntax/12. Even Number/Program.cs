a3e8eedda8f10eeb1b5be048c4846825ad4db22d
Kris Yovchev <105774870+krisyovchevv@users.noreply.github.com>
2022-05-19 15:26:19 +0300

    Add files via upload

---
+++ Intro and basic syntax/12. Even Number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _12._Even_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isOdd = true;
+            while (isOdd)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (num % 2 == 0)
+                {
+                    Console.WriteLine($"The number is: {Math.Abs(num)}");
+                    isOdd = false;
+                }
+                else
+                {
+                    Console.WriteLine("Please write an even number.");
+                }
+            }
+        }
+    }
+}
