e99e3958d5f3a82b4979686f0f5452bef4015f49
Kris Yovchev <105774870+krisyovchevv@users.noreply.github.com>
2022-05-20 10:03:38 +0300

    Add files via upload

---
+++ Ex-Intro and basic syntax/7. Vending Machine/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace _7._Vending_Machine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double money = 0;
+            double insertedCoins = 0;
+
+            while (input != "Start")
+            {
+                double coin = double.Parse(input);
+                bool suppCoins = coin == 0.1 || coin == 0.2 || coin == 0.5 || coin == 1 || coin == 2;
+                if (suppCoins == false)
+                {
+                    Console.WriteLine($"Cannot accept {coin}");
+                    input = Console.ReadLine();
+                    continue;
+                }
+                else
+                {
+                    money += coin;
+                    input = Console.ReadLine();
+                }
+            }
+            string input2 = Console.ReadLine();
+
+            while (input2 != "End")
+            {
+                switch (input2)
+                {
+                    case "Nuts":
+                        if (money < 2)
+                        {
+                            Console.WriteLine("Sorry, not enough money");
+                        }
+                        else
+                        {
+                            money -= 2;
+                            Console.WriteLine("Purchased nuts");
+                        }
+                        break;
+                    case "Water":
+                        if (money < 0.7)
+                        {
+                            Console.WriteLine("Sorry, not enough money");
+                        }
+                        else
+                        {
+                            money -= 0.7;
+                            Console.WriteLine("Purchased water");
+                        }
+                        break;
+                    case "Crisps":
+                        if (money < 1.5)
+                        {
+                            Console.WriteLine("Sorry, not enough money");
+                        }
+                        else
+                        {
+                            money -= 1.5;
+                            Console.WriteLine("Purchased crisps");
+                        }
+                        break;
+                    case "Soda":
+                        if (money < 0.8)
+                        {
+                            Console.WriteLine("Sorry, not enough money");
+                        }
+                        else
+                        {
+                            money -= 0.8;
+                            Console.WriteLine("Purchased soda");
+                        }
+                        break;
+                    case "Coke":
+                        if (money < 1)
+                        {
+                            Console.WriteLine("Sorry, not enough money");
+                        }
+                        else
+                        {
+                            money -= 1;
+                            Console.WriteLine("Purchased coke");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Invalid product");
+                        break;
+                }
+                input2 = Console.ReadLine();
+            }
+            Console.WriteLine($"Change: {Math.Abs(money):f2}");
+        }
+    }
+}
---
+++ Ex-Intro and basic syntax/8. Triangle of Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _8._Triangle_of_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int c = 1; c <= n; c++)
+            {
+                for (int r = 1; r <= c; r++)
+                {
+                    Console.Write(c + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
