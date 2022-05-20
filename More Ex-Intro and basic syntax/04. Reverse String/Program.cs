8f6e5fec6e821c425555a5c81c98055589e49dfa
Kris Yovchev <105774870+krisyovchevv@users.noreply.github.com>
2022-05-20 17:30:06 +0300

    Add files via upload

---
+++ More Ex-Intro and basic syntax/04. Reverse String/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _04._Reverse_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int numLetters = text.Length;
+            for (int i = numLetters - 1; i >= 0; i--)
+            {
+                Console.Write(text[i]);
+            }
+        }
+    }
+}
