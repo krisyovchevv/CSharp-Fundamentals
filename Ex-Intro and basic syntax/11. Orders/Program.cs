48b44c5ef0adced82577c425cbbc29c4128c7885
Kris Yovchev <105774870+krisyovchevv@users.noreply.github.com>
2022-05-20 12:01:39 +0300

    Add files via upload

---
+++ Ex-Intro and basic syntax/11. Orders/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _11._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int orders = int.Parse(Console.ReadLine());
+            double totalPrice = 0;
+
+            for (int i = 1; i <= orders; i++)
+            {
+                double pricePerCapsule = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                int capsulesCount = int.Parse(Console.ReadLine());
+                double price = ((days * capsulesCount) * pricePerCapsule);
+                totalPrice += price;
+                Console.WriteLine($"The price for the coffee is: ${price:f2}");
+            }
+            Console.WriteLine($"Total: ${totalPrice:f2}");
+        }
+    }
+}
