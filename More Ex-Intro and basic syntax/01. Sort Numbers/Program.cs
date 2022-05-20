dddf8f640fe574b504a192a6b10ac1cb96061f5c
Kris Yovchev <105774870+krisyovchevv@users.noreply.github.com>
2022-05-20 17:28:29 +0300

    Add files via upload

---
+++ More Ex-Intro and basic syntax/01. Sort Numbers/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _01._Sort_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+            
+            if (n1 >= n2 && n1 >= n3)
+            {
+                if (n2 >= n3)
+                {
+                    Console.WriteLine(n1);
+                    Console.WriteLine(n2);
+                    Console.WriteLine(n3);
+                }
+                else if (n3 >= n2)
+                {
+                    Console.WriteLine(n1);
+                    Console.WriteLine(n3);
+                    Console.WriteLine(n2);
+                }
+            }
+            else if (n2 >= n1 && n2 >= n3)
+            {
+                if (n1 >= n3)
+                {
+                    Console.WriteLine(n2);
+                    Console.WriteLine(n1);
+                    Console.WriteLine(n3);
+                }
+                else if (n1 <= n3)
+                {
+                    Console.WriteLine(n2);
+                    Console.WriteLine(n3);
+                    Console.WriteLine(n1);
+                }
+            }
+            else if (n3 >= n1 && n3 >= n2)
+            {
+                if (n1 >= n2)
+                {
+                    Console.WriteLine(n3);
+                    Console.WriteLine(n1);
+                    Console.WriteLine(n2);
+                }
+                else if (n1 <= n2)
+                {
+                    Console.WriteLine(n3);
+                    Console.WriteLine(n2);
+                    Console.WriteLine(n1);
+                }
+            }
+        }
+    }
+}
