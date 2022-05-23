97fae6f96080be37a8b1a50f26b1c70cabbeba41
Kris Yovchev <105774870+krisyovchevv@users.noreply.github.com>
2022-05-23 23:58:10 +0300

    Update Program.cs

--- Ex - Data Types and Variables/06. Triples of Latin Letters/Program.cs
+++ Ex - Data Types and Variables/06. Triples of Latin Letters/Program.cs
@@ -6,15 +6,17 @@ namespace _06._Triples_of_Latin_Letters
     {
         static void Main(string[] args)
         {
-            for (int i = 97; i <= 99; i++)
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
             {
-                for (int j = 97; j <= 99; j++)
+                for (int j = 0; j < n; j++)
                 {
-                    for (int l = 97; l <= 99; l++)
+                    for (int l = 0; l < n; l++)
                     {
-                        char ch1 = (char)i;
-                        char ch2 = (char)j;
-                        char ch3 = (char)l;
+                        char ch1 = (char)('a' + i);
+                        char ch2 = (char)('a' + j);
+                        char ch3 = (char)('a' + l);
                         Console.WriteLine($"{ch1}{ch2}{ch3}");
                     }
                 }
