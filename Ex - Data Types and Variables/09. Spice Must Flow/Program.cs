c9b6f775905e7348087467030e80db367a90a594
Kris Yovchev <105774870+krisyovchevv@users.noreply.github.com>
2022-05-24 01:35:27 +0300

    Updated bug in 09.Spice Must Flow

--- Ex - Data Types and Variables/09. Spice Must Flow/Program.cs
+++ Ex - Data Types and Variables/09. Spice Must Flow/Program.cs
@@ -6,7 +6,29 @@ namespace _09._Spice_Must_Flow
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int startingYield = int.Parse(Console.ReadLine());
+            int yield = startingYield;
+            int totalSpices = 0;
+            int workingDays = 0;
+
+            while (yield >= 100) 
+            {
+                int spices = yield;
+                yield -= 10;
+                spices -= 26;
+                totalSpices += spices;
+                workingDays++;                
+            } 
+            if (totalSpices >= 26)
+            {
+                totalSpices -= 26;
+            }
+            else
+            {
+                totalSpices = 0;
+            }
+            Console.WriteLine(workingDays);
+            Console.WriteLine(totalSpices);
         }
     }
 }
