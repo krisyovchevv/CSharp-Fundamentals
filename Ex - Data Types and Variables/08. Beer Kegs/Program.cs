59e72cdfa2e6103e618552249b4a7cbef22c2e02
Kris Yovchev <105774870+krisyovchevv@users.noreply.github.com>
2022-05-24 00:32:06 +0300

    Ex

---
+++ Ex - Data Types and Variables/08. Beer Kegs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _08._Beer_Kegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte n = byte.Parse(Console.ReadLine());
+            string biggestVolumeName = null;
+            double biggestVolume = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                string typeKeg = Console.ReadLine();
+                double radiusOfKeg = double.Parse(Console.ReadLine());
+                double heightOfKeg = int.Parse(Console.ReadLine());
+                double volume = Math.PI * (radiusOfKeg * radiusOfKeg) * heightOfKeg;
+                if (biggestVolume < volume)
+                {
+                    biggestVolumeName = typeKeg;
+                    biggestVolume = volume;
+                }
+            }
+            Console.WriteLine(biggestVolumeName);
+        }
+    }
+}
