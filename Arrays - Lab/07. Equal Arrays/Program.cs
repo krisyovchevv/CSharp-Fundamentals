05cec735b9a9893be34ffca69442222e951b78c9
Kris Yovchev <105774870+krisyovchevv@users.noreply.github.com>
2022-06-01 19:05:54 +0300

    LATEST EX AHHHH

---
+++ Arrays - Lab/07. Equal Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Equal_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstNum = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var secondNum = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+            int counter = 0;
+            for (int i = 0; i < firstNum.Length; i++)
+            {
+                if (firstNum[i] != secondNum[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    sum = 0;
+                    break;
+                }
+                else
+                {
+                    sum += firstNum[i];
+                }
+            }
+            if (sum > 0)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }  
+        }
+    }
+}
---
+++ Arrays - Lab/08. Condense Array to Number/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Condense_Array_to_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int i = 0;
+            int couter = 0;
+            while (nums.Length > 1 && i < nums.Length)
+            {
+                int indexToRemove = nums[i];
+                if (nums.Length == i + 1)
+                {
+                    nums = nums.Where(x => x > indexToRemove).ToArray();
+                    i = 0;
+                }
+                if (nums.Length == 1)
+                {
+                    break;
+                }
+                couter++;
+                int condensed = nums[i] + nums[i + 1];
+                nums[i] = condensed;
+                i++;
+            }
+            Console.WriteLine(nums[0]);
+        }
+    }
+}
