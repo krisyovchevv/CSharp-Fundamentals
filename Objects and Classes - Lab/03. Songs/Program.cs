b2fc490e0c46264de0e3acedefd10ef32ec1d8dc
Kris Yovchev <105774870+krisyovchevv@users.noreply.github.com>
2022-07-03 23:16:16 +0300

    objects 2.0

---
+++ Objects and Classes - Lab/03. Songs/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Songs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numSongs = int.Parse(Console.ReadLine());
+            List<Songs> songs = new List<Songs>();
+            for (int i = 0; i < numSongs; i++)
+            {
+                string[] input = Console.ReadLine().Split("_");
+
+                string typeList = input[0];
+                string name = input[1];
+                string time = input[2];
+
+                Songs song = new Songs(typeList, name, time);
+
+                songs.Add(song);
+            }
+            string operation = Console.ReadLine();
+            if (operation == "all")
+            {
+                foreach (var song in songs)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+            else
+            {
+                foreach (var song in songs)
+                {
+                    if (song.TypeList == operation)
+                    {
+                    Console.WriteLine(song.Name);
+                    }
+                }
+            }
+        }
+    }
+    class Songs
+    {
+        public Songs(string typeList, string name, string time)
+        {
+            this.TypeList = typeList;
+            this.Name = name;
+            this.Time = time;
+        }
+        public string TypeList { get; set; }
+        public string Name { get; set; }
+        public string Time { get; set; }
+    }
+}
---
+++ Objects and Classes - Lab/04. Students/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+            List<Student> students = new List<Student>();
+            while (inputs[0] != "end")
+            {
+                int age = int.Parse(inputs[2]);
+                Student student = new Student(inputs[0], inputs[1], age, inputs[3]);
+                students.Add(student);
+                inputs = Console.ReadLine().Split(' ');
+            }
+            string city = Console.ReadLine();
+            foreach (Student student in students)
+            {
+                if (city == student.HomeTown)
+                {
+                    Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+                }
+            }
+        }
+
+        class Student
+        {
+            public Student(string firstName, string lastName, int age, string homeTown)
+            {
+                this.FirstName = firstName;
+                this.LastName = lastName;
+                this.Age = age;
+                this.HomeTown = homeTown;
+            }
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+            public int Age { get; set; }
+
+            public string HomeTown { get; set; }
+
+        }
+    }
+}
